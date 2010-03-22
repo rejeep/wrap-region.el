40f2e7659aebaea9aa8ebb9a0922315308ecaaf2
Johan Andersson <johan.rejeep@gmail.com>
2010-03-22 10:29:23 +0000

    Added lisp file comments.

--- wrap-region.el
+++ wrap-region.el
@@ -32,21 +32,34 @@
 ;; wrap-region is a minor mode that wraps text with punctuations. For
 ;; tagged markup modes, such as HTML and XML, it wraps a region
 ;; with a tag instead of punctuations.
-
-;; To use wrap-region, make sure that this file is in Emacs load-path
+;;
+;; To use wrap-region, make sure that this file is in Emacs load-path:
 ;; (add-to-list 'load-path "/path/to/directory/or/file")
 ;;
-;; Then require wrap-region
+;; Then require wrap-region:
 ;; (require 'wrap-region)
-
-;; To start wrap-region
+;;
+;;
+;; To start wrap-region:
 ;; (wrap-region-mode t) or M-x wrap-region-mode
 ;;
-;; If you only want wrap-region active in some mode, use a hook
+;; If you only want wrap-region active in some mode, use a hook:
 ;; (add-hook 'ruby-mode-hook 'wrap-region-mode)
 ;;
-;; Or if you want it activate in all buffers, use the global mode.
+;; Or if you want to activate it in all buffers, use the global mode:
 ;; (wrap-region-global-mode t)
+;;
+;;
+;; To wrap a region, select that region and hit one of the punctuation
+;; keys. In "tag-modes" (html-mode, sgml-mode or xml-mode), "<" is
+;; replaced and wraps the region with a tag. To activate this behavior
+;; in a mode other than default, you do:
+;; (add-to-list 'wrap-region-tag-active-modes 'some-tag-mode)
+;;
+;; `wrap-region-punctuations-table' contains a few default
+;; punctuations that wraps. You can add you own like this:
+;; (wrap-region-add-punctuation "#" "#")
+ 
 
 ;;; Code:
 
