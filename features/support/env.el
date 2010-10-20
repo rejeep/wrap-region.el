bfcb53d1d9f8bde9c4df38463fda10819466ac9f
Johan Andersson <johan.rejeep@gmail.com>
2010-10-20 13:19:01 +0200

    Move support.el => support/env.el

---
+++ features/support/env.el
@@ -0,0 +1,42 @@
+(let* ((current-directory (file-name-directory load-file-name))
+       (features-directory (expand-file-name ".." current-directory))
+       (project-directory (expand-file-name ".." features-directory)))
+  (setq enclose-root-path project-directory)
+  (setq enclose-util-path (expand-file-name "util" project-directory)))
+
+(add-to-list 'load-path enclose-root-path)
+(add-to-list 'load-path (expand-file-name "espuds" enclose-util-path))
+(add-to-list 'load-path (expand-file-name "emacs-lisp" (expand-file-name "lisp" (expand-file-name "ert" enclose-util-path))))
+
+(require 'wrap-region)
+(require 'espuds)
+(require 'ert)
+
+(setq default-except-modes wrap-region-except-modes)
+
+(Before
+ ;; Start with a clean slate.
+ (switch-to-buffer (get-buffer-create "*wrap-region*"))
+ (erase-buffer)
+ (transient-mark-mode 1)
+ (deactivate-mark))
+
+(After
+ ;; For scenarios that add and remove wrapper.
+ (wrap-region-add-wrapper "(" ")")
+ (wrap-region-remove-wrapper "<")
+
+ ;; Reset hooks
+ (setq wrap-region-hook '())
+ (setq wrap-region-before-insert-twice-hook '())
+ (setq wrap-region-after-insert-twice-hook '())
+ (setq wrap-region-before-wrap-hook '())
+ (setq wrap-region-after-wrap-hook '())
+
+ ;; Disable wrap-region-mode
+ (wrap-region-mode -1)
+ (wrap-region-global-mode -1)
+
+ ;; Reset all except modes
+ (setq wrap-region-except-modes default-except-modes)
+ )
