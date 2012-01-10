de7de07a9f958f9450429748fe8fe7ef2bf343ea
Johan Andersson <johan.rejeep@gmail.com>
2012-01-10 15:16:41 +0100

    Fix load path.

--- features/support/env.el
+++ features/support/env.el
@@ -1,12 +1,12 @@
 (let* ((current-directory (file-name-directory load-file-name))
        (features-directory (expand-file-name ".." current-directory))
        (project-directory (expand-file-name ".." features-directory)))
-  (setq enclose-root-path project-directory)
-  (setq enclose-util-path (expand-file-name "util" project-directory)))
+  (setq wrap-region-root-path project-directory)
+  (setq wrap-region-util-path (expand-file-name "util" project-directory)))
 
-(add-to-list 'load-path enclose-root-path)
-(add-to-list 'load-path (expand-file-name "espuds" enclose-util-path))
-(add-to-list 'load-path (expand-file-name "emacs-lisp" (expand-file-name "lisp" (expand-file-name "ert" enclose-util-path))))
+(add-to-list 'load-path wrap-region-root-path)
+(add-to-list 'load-path (expand-file-name "espuds" wrap-region-util-path))
+(add-to-list 'load-path (expand-file-name "emacs-lisp" (expand-file-name "lisp" (expand-file-name "ert" wrap-region-util-path))))
 
 (require 'wrap-region)
 (require 'espuds)
