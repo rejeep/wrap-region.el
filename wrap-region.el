83da37ef18cd3308659006c100928985949d2d38
Johan Andersson <johan.rejeep@gmail.com>
2012-02-07 15:42:12 +0100

    Update version to 0.5.0

--- wrap-region.el
+++ wrap-region.el
@@ -4,7 +4,7 @@
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.4.0
+;; Version: 0.5.0
 ;; Keywords: speed, convenience
 ;; URL: http://github.com/rejeep/wrap-region
 
